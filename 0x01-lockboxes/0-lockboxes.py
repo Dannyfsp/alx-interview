0028d4f3dcd108aa1857527db5f4427e04f58ab4
Dannyfsp <femoscopilala@gmail.com>
2023-04-28 04:02:17 +0000

    method that determines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,14 +17,14 @@ def canUnlockAll(boxes):
     check = [0]
     all_list = [i for i in range(len(boxes))]
 
-    for i in check:
-        for y in boxes[i]:
+    for x in check:
+        for y in boxes[x]:
             if y not in check and y in all_list:
                 if y >= len(boxes):
                     return False
                 check.append(y)
 
-    if len(check) == len(check):
+    if len(check) == len(boxes):
         return True
     else:
         return False
